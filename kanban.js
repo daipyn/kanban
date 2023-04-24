a48344583b82e4218ecd17013ca34f2efd20f6e1
daipyn <2029094@kiit.ac.in>
2023-04-25 03:22:05 +0530

    Second Commit

--- kanban.js
+++ kanban.js
@@ -1,116 +1,130 @@
 export default class Kanban{
-    // Get task, fetch task, update task, delete task, get all task 
 
     static getTasks(columnId){
-        //const data=read(); // now you have information for all the columns
-
-        const data=read().find(column=>{
-            return column.columnId==columnId;
+        const data = read().find(column => {
+            return column.columnId == columnId;
         });
-        if(!data){
-            return [];
-        }
+        
         return data.tasks;
-
-        /*
-            Another way
-            const data=read()[columnId];
-        if(!data){
-            return [];
-        }
-        return data.tasks;
-
-            
-        */
     }
 
-    static insertTask(columnId,content){
-        const data=read();
-        const currentcolumn=data.find(column=>{
-            return column.columnId==columnId;
-        }); // giving access to the column now you can add data
+    static insertTask(columnId, content){
+        const data = read();
+        const column = data.find(column => {
+            return column.columnId == columnId;
+        });
         const task = {
-            taskId:Math.floor(Math.random() * 1000000),
-            content:content // Given by user
+            taskId: Math.floor(Math.random() * 100000),
+            content: content
         };
 
-        currentcolumn.tasks.push(task); // Task has been added
-        localStorage.setItem("data",JSON.stringify(data)); //Updated data and saved in local storage
+        column.tasks.push(task);
+        console.log(data);
+        save(data);
+
         return task;
     }
 
-    static deleteTask(taskId){
-        const data=read();
-        for(const column of data){
-            const task=column.tasks.find(item =>{
-                return item.taskId== taskId;
-            });
+    static updateTask(taskId, updatedInformation){
+        const data = read();
 
-            if(task){
-                column.tasks.splice(column.tasks.indexOf(task),1);
-            }
-        }
-        save(data);
-    }
-
-    static updateTask(taskId, newcontent){
-        const data=read();
-
-        function findColumnTask(){ // Find the current information 
+        function findColumnTask(){
             for(const column of data){
-                const task=column.tasks.find(item=>{
-                    return item.taskId==taskId;
+                const task = column.tasks.find(item => {
+                    return item.taskId == taskId;
                 });
+    
                 if(task){
-                    return[task,column]; // return the information of the found task
+                    return [task, column];
                 }
             }
         }
-        const [task,currentcolumn]=findColumnTask();  // task and currentcolumn variables get the content and columnId
-        
-        const targetcolumn=data.find(column=>{
-            return column.columnId==newcontent.columnId;
-        }); // Where you need to update it
+        const [task, currentColumn] = findColumnTask();
 
-        task.content=newcontent.content; // copied the contents
-        currentcolumn.tasks.splice(currentcolumn.tasks.indexOf(task),1); // delete from the old 
-        targetcolumn.tasks.push(task); // push the task with updated content to the required columnId
+        const targetColumn = data.find(column => {
+            return column.columnId == updatedInformation.columnId;
+        });
+
+        task.content = updatedInformation.content;
+        currentColumn.tasks.splice(currentColumn.tasks.indexOf(task), 1);
+        targetColumn.tasks.push(task);
 
         save(data);
     }
 
-    static getallTask(){
-        const data=read();
-        columncount();
-        return [data[0].tasks,data[1].tasks,data[2].tasks];
+    static deleteTask(taskId){
+        const data = read();
+
+        for(const column of data){
+            const task = column.tasks.find(item => {
+                return item.taskId == taskId;
+            });
+
+            if(task){
+                column.tasks.splice(column.tasks.indexOf(task), 1);
+            }            
+        }
+
+        save(data);
+    }
+
+    static getAllTasks(){
+        const data = read();
+        columnCount();
+        return [data[0].tasks, data[1].tasks, data[2].tasks];
     }
 }
 
 function read(){
-    const data=localStorage.getItem("data");
+    const data = localStorage.getItem("data");
+
     if(!data){
         return [
-            {columnId:0, tasks:[]},
-            {columnId:1, tasks:[]},
-            {columnId:2, tasks:[]}
+            {columnId: 0, tasks: []}, 
+            {columnId: 1, tasks: []}, 
+            {columnId: 2, tasks: []}
         ];
     }
+
     return JSON.parse(data);
 }
 
 function save(data){
-    localStorage.setItem("data",JSON.stringify(data));
-    columncount()
+    localStorage.setItem("data", JSON.stringify(data));
+    columnCount();
 }
 
-function columncount(){
-    const data=read();
-    const todo=document.querySelector("span.todo");
-    todo.textContent=data[0].tasks.length;
+function columnCount(){
+    const data = read();
 
-    const pending=document.querySelector("span.pending");
-    pending.textContent=data[1].tasks.length;
+    const todo = document.querySelector("span.todo");
+    todo.textContent = data[0].tasks.length;
 
-    const completed=document.querySelector("span.completed");
-    completed.textContent=data[2].tasks.length;
+    const pending = document.querySelector("span.pending");
+    pending.textContent = data[1].tasks.length;
+
+    const completed = document.querySelector("span.completed");
+    completed.textContent = data[2].tasks.length;
 }
+
+
+// console.log(Kanban.getAllTasks());
+// console.log(Kanban.getTasks(1));
+
+
+// console.log(Kanban.getTasks(1));
+// console.log(Kanban.insertTask(0, "Record Kanban Lectures"));
+// console.log(Kanban.getTasks(1));
+
+
+// console.log(Kanban.getAllTasks());
+// Kanban.deleteTask(11822);
+// console.log(Kanban.getAllTasks());
+
+
+// console.log(Kanban.getAllTasks());
+// Kanban.updateTask(97522, {
+//     columnId: 2,
+//     content: "Record JavaScript Preview"
+// });
+// console.log(Kanban.getAllTasks());
