50e0fc2a76617a2b28d6be214c8fd5de7decc72b
daipyn <2029094@kiit.ac.in>
2023-04-25 14:34:09 +0530

    background change

--- index.js
+++ index.js
@@ -102,3 +102,13 @@ taskbox.forEach(column => {
         }
     });
 });
+
+const handlemousemove=e=>{
+    const {currenttarget: target}=e;
+    const rect=target.getBoundingClientReact(),
+    x=e.clientX-rect.left,
+    y=e.clientY-rect.top;
+    
+    target.style.setProperty("--mouse-x",`${x}px`);
+    target.style.setProperty("--mouse-y",`${y}px`);
+}
